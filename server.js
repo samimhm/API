d8e0c20c7c56f16f6963b8bb9419b11ce9a4dcf3
samimhm <mhm.sami@yahoo.com>
2020-03-24 18:54:02 +0200

    resolving worldometer tabel changes

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ let getcountries = async () => {
     .children("td");
 
   // NOTE: this will change when table format change in website
-  const totalColumns = 9;
+  const totalColumns = 10;
   const countryColIndex = 0;
   const casesColIndex = 1;
   const todayCasesColIndex = 2;
